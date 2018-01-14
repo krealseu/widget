5ea5e0b0ef73608e13f7ff4c0ce74ef1bef190c5
CC <ltee12@hotmail.com>
2018-01-14 18:22:30 +0800

    更新测试的界面

--- app/src/main/java/org/kreal/widget/MainActivity.kt
+++ app/src/main/java/org/kreal/widget/MainActivity.kt
@@ -3,20 +3,27 @@ package org.kreal.widget
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.TextView
+import kotlinx.android.synthetic.main.activity_main.*
 import org.kreal.widget.filepickdialog.FilePickDialogFragment
 
 class MainActivity : AppCompatActivity() {
-
+    private lateinit var resultView: TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        FilePickDialogFragment().apply {
-            selectFolder = true
-            setListener {
-                it.forEach {
-                    Log.i("jj", it)
+        resultView = result_view
+        test_file_pick.setOnClickListener {
+            FilePickDialogFragment().apply {
+                selectFolder = true
+                setListener {
+                    var string: String = ""
+                    it.forEach {
+                        string += it
+                    }
+                    resultView.text = string
                 }
-            }
-        }.show(fragmentManager, "l")
+            }.show(fragmentManager, "l")
+        }
     }
 }
